42c741a1811ae852e146db8a47fed9f6e5cc9519
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2023-05-29 20:26:46 +0700

    Remote lampu

--- program enkapsulasi/program enkapsulasi.cpp
+++ program enkapsulasi/program enkapsulasi.cpp
@@ -13,18 +13,3 @@ public:
 	}
 };
 
-int main() {
-	remoteLampu lampuRumah;
-
-	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
-	lampuRumah.setSaklarNo(1, "Lampu Rumah Tamu");
-	lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
-	lampuRumah.setSaklarNo(3, "Lampu Dapur");
-
-	cout << lampuRumah.getsaklarno(0) << endl;
-	cout << lampuRumah.getsaklarno(1) << endl;
-	cout << lampuRumah.getsaklarno(2) << endl;
-	cout << lampuRumah.getsaklarno(3) << endl;
-
-	return 0;
-}
